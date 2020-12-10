b6e1da2255a43e2043adfb696528c6d8f040cd40
Emilio <epelaaez@gmail.com>
2020-12-10 13:58:16 +0100

    Expand swap test to include multi-qubit registers

--- main.py
+++ main.py
@@ -39,19 +39,22 @@ def swap_test():
     """
     The swap test circuit can tell us if two registers are in the same state without measuring them. However, it cannot tell us what state they are in. 
     """
-    qreg_1  = QuantumRegister(1, 'q1')
-    qreg_2  = QuantumRegister(1, 'q2')
+    qreg_1  = QuantumRegister(2, 'q1')
+    qreg_2  = QuantumRegister(2, 'q2')
     ancilla = QuantumRegister(1, 'a')
     creg    = ClassicalRegister(1, 'c')
     qc      = QuantumCircuit(qreg_1, qreg_2, ancilla, creg)
 
     # Initialize first two quantum registers to desired state
     qc.h(qreg_1[0])
+    qc.x(qreg_1[1])
     qc.h(qreg_2[0])
+    qc.x(qreg_2[1])
 
     # Swap test 
     qc.h(ancilla[0])
-    qc.cswap(ancilla[0], qreg_1[0], qreg_2[0])
+    for i in range(len(qreg_1)):
+        qc.cswap(ancilla[0], qreg_1[i], qreg_2[i])
     qc.h(ancilla[0])
     qc.x(ancilla[0])
 
