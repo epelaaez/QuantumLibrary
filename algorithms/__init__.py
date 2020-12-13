db0b27302807843554fe73c26b1f7527c5da6d40
Emilio <epelaaez@gmail.com>
2020-12-13 13:56:31 +0100

    Add quantum teleportation

--- algorithms/__init__.py
+++ algorithms/__init__.py
@@ -1,9 +1,11 @@
 from os.path import join
 
+import math
+
 from qiskit import *
 from qiskit.visualization import plot_histogram, circuit_drawer, plot_bloch_multivector
 
 from .bell_state.bell_state import create_bell_state
 from .swap_test.swap_test import swap_test
 from .phase_kickback.phase_kickback import phase_kickback
-from .even_oracle.even_oracle import even_oracle
+from .teleportation.teleportation import teleportation
--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def main():
     except NameError:
         raise Exception("You have not set up your config file correctly.")
 
-    even_oracle()
+    teleportation()
 
 if __name__ == "__main__":
     main()
